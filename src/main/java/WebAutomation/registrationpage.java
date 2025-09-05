6814c03ac112dcead42f8d54be21472d53b0eb9a
pratiksha <pratikshabarde2002@gmail.com>
2025-09-05 11:51:59 +0530

    newfile

---
+++ src/main/java/WebAutomation/registrationpage.java
@@ -0,0 +1,17 @@
+package WebAutomation;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.Test;
+
+public class registrationpage {
+	@Test
+	public void homePage() {
+		
+		WebDriver driver = new EdgeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.google.com/");
+		driver.close();
+	}
+	
+}
